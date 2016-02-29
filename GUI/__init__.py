d7741b9e301976b3d43bc3d97ff408e699aac2ab
Jesus Garlea <chamoysvoice@gmail.com>
2016-02-29 17:05:21 -0600

    Color hist :)

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -47,17 +47,22 @@ def main_window():
     global_variables['gui_image'] = label.image
     label.grid(row = 0, column= 0, rowspan=12)
     tk.Button(main_gui_window, text="Convertir a escala de grises", command=convert_to_grayscale_gui).grid(row=0, column=1, sticky="NEWS")
-    tk.Button(main_gui_window, text="Convertir a imagen Binaria", command=convert_to_binary_gui).grid(row=1, column=1, sticky="NEWS")
+    tk.Button(main_gui_window, text="Convertir a imagen Binaria", command=convert_to_binary_ask).grid(row=1, column=1, sticky="NEWS")
     tk.Button(main_gui_window, text="Invertir colores", command=invert_image_gui).grid(row=2, column=1, sticky="NEWS")
+
+    tk.Button(main_gui_window, text="Ver Histograma", command=show_hist).grid(row=3, column=1, sticky="NEWS")
+
 
     main_gui_window.mainloop()
 
+def show_hist():
+    hist.hist(global_variables['working_image'])
 
 def update_image(img):
+    global_variables['working_image'] = img
     photo = ImageTk.PhotoImage(img)
     global_variables['image_label'].configure(image=photo)
     global_variables['image_label'].image = photo
-    hist.hist(img)
 
 def convert_to_grayscale_gui():
     img = general.convert_to_grayscale(global_variables['working_image'])
@@ -65,8 +70,17 @@ def convert_to_grayscale_gui():
 
 
 def convert_to_binary_gui():
-    img = general.binary_threshold(global_variables['working_image'])
+    img = general.binary_threshold(global_variables['working_image'], int(global_variables['binary_scale'].get()))
     update_image(img)
+
+def convert_to_binary_ask():
+    option_pane = tk.Tk()
+    tk.Label(option_pane, text="Select Threshold").pack()
+    global_variables['binary_scale'] = tk.Scale(option_pane, from_=0, to=255, orient=tk.HORIZONTAL)
+    global_variables['binary_scale'].pack()
+    tk.Button(option_pane, text="Preview").pack()
+    tk.Button(option_pane, text="Apply", command=convert_to_binary_gui).pack()
+
 
 
 def invert_image_gui():
--- utils/hist.py
+++ utils/hist.py
@@ -4,11 +4,13 @@ def hist(img):
     freq = img.histogram()
     h = Image.new('RGB', (256, 256), "black")
     avg = sum(freq) / len(freq)
+    c = [0, 0, 0]
     for y in xrange(h.size[1]):
         for x in xrange(h.size[0]):
-            if freq[y] * 256 / avg > x :
-                c = 255
-            else:
-                c = 0
-            h.putpixel((255 - y, 255 - x),  (c, c, c))
+            for k in xrange(len(c)):
+                if freq[y + (256 * k)] * 256 / (avg * 1.5) > x :
+                    c[k] = min(128 + x / 2, 255)
+                else:
+                    c[k] = 0
+            h.putpixel((y, 255 - x),  (c[0], c[1], c[2]))
     h.show()
